5bf67ed5f4591ad3570f31fcfcd7e3cd435d437f
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-02-25 12:10:54 +0100

    cleanup and fix tests

--- src/hotspot/share/oops/cpCache.cpp
+++ src/hotspot/share/oops/cpCache.cpp
@@ -451,13 +451,13 @@ void ConstantPoolCache::remove_resolved_field_entries_if_non_deterministic() {
       Symbol* name = cp->uncached_name_ref_at(cp_index);
       Symbol* signature = cp->uncached_signature_ref_at(cp_index);
       if (resolved) {
-        log.print("%s field  CP entry [%3d]: %s => %s.%s:%s%s %s",
+        log.print("%s field  CP entry [%3d]: %s => %s.%s:%s%s%s",
                   (archived ? "archived" : "reverted"),
                   cp_index,
                   cp->pool_holder()->name()->as_C_string(),
                   klass_name->as_C_string(), name->as_C_string(), signature->as_C_string(),
                   rfi->is_resolved(Bytecodes::_getstatic) || rfi->is_resolved(Bytecodes::_putstatic) ? " *** static" : "",
-                  (archived ? "" : "(resolution is not deterministic)"));
+                  (archived ? "" : " (resolution is not deterministic)"));
       }
     }
     ArchiveBuilder::alloc_stats()->record_field_cp_entry(archived, resolved && !archived);
@@ -493,7 +493,7 @@ void ConstantPoolCache::remove_resolved_method_entries_if_non_deterministic() {
       Symbol* name = cp->uncached_name_ref_at(cp_index);
       Symbol* signature = cp->uncached_signature_ref_at(cp_index);
       if (resolved) {
-        log.print("%s%s method CP entry [%3d]: %s %s.%s:%s %s",
+        log.print("%s%s method CP entry [%3d]: %s %s.%s:%s%s",
                   (archived ? "archived" : "reverted"),
                   (rme->is_resolved(Bytecodes::_invokeinterface) ? " interface" : ""),
                   cp_index,
@@ -539,9 +539,9 @@ void ConstantPoolCache::remove_resolved_indy_entries_if_non_deterministic() {
         log.print("%s indy   CP entry [%3d]: %s (%d)",
                   (archived ? "archived" : "reverted"),
                   cp_index, cp->pool_holder()->name()->as_C_string(), i);
-        log.print(" %s %s.%s:%s %s", (archived ? "=>" : "  "), bsm_klass->as_C_string(),
+        log.print(" %s %s.%s:%s%s", (archived ? "=>" : "  "), bsm_klass->as_C_string(),
                   bsm_name->as_C_string(), bsm_signature->as_C_string(),
-                  (archived ? "" : "(resolution is not deterministic)"));
+                  (archived ? "" : " (resolution is not deterministic)"));
       }
     }
     ArchiveBuilder::alloc_stats()->record_indy_cp_entry(archived, resolved && !archived);
@@ -549,37 +549,36 @@ void ConstantPoolCache::remove_resolved_indy_entries_if_non_deterministic() {
 }
 
 bool ConstantPoolCache::can_archive_resolved_method(ConstantPool* src_cp, ResolvedMethodEntry* method_entry, const char*& reason) {
-  LogStreamHandle(Trace, aot, resolve) log;
   InstanceKlass* pool_holder = constant_pool()->pool_holder();
   if (pool_holder->defined_by_other_loaders()) {
     // Archiving resolved cp entries for classes from non-builtin loaders
     // is not yet supported.
-    reason = "(pool holder comes from a non-builtin loader)";
+    reason = " (pool holder comes from a non-builtin loader)";
     return false;
   }
 
   if (CDSConfig::is_dumping_dynamic_archive()) {
     // InstanceKlass::methods() has been resorted. We need to
     // update the vtable_index in method_entry (not implemented)
-    reason = "(InstanceKlass::methods() has been resorted)";
+    reason = " (InstanceKlass::methods() has been resorted)";
     return false;
   }
 
   if (!method_entry->is_resolved(Bytecodes::_invokevirtual)) {
     if (method_entry->method() == nullptr) {
-      reason = "(method entry is not resolved)";
+      reason = " (method entry is not resolved)";
       return false;
     }
     if (method_entry->method()->is_continuation_native_intrinsic()) {
-      reason = "(corresponding stub is generated on demand during method resolution)";
+      reason = " (corresponding stub is generated on demand during method resolution)";
       return false; // FIXME: corresponding stub is generated on demand during method resolution (see LinkResolver::resolve_static_call).
     }
     if (method_entry->is_resolved(Bytecodes::_invokehandle) && !CDSConfig::is_dumping_method_handles()) {
-      reason = "(not dumping method handles)";
+      reason = " (not dumping method handles)";
       return false;
     }
     if (method_entry->method()->is_method_handle_intrinsic() && !CDSConfig::is_dumping_method_handles()) {
-      reason = "(not dumping intrinsic method handles)";
+      reason = " (not dumping intrinsic method handles)";
       return false;
     }
   }
@@ -588,7 +587,7 @@ bool ConstantPoolCache::can_archive_resolved_method(ConstantPool* src_cp, Resolv
   assert(src_cp->tag_at(cp_index).is_method() || src_cp->tag_at(cp_index).is_interface_method(), "sanity");
 
   if (!AOTConstantPoolResolver::is_resolution_deterministic(src_cp, cp_index)) {
-      reason = "(resolution is not deterministic)";
+      reason = " (resolution is not deterministic)";
     return false;
   }
   return true;
